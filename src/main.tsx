a960a8873993c105372ffcf340a8222dfefacc0b
Raul Clauson <106999644+RaulClauson@users.noreply.github.com>
2025-02-03 23:45:56 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,27 @@
 import { Analytics } from "@vercel/analytics/react";
-import { StrictMode, useState } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Header from "./components/Header/Header.tsx";
 import PageLoading from "./components/PageLoading/PageLoading.tsx";
 import CustomCursor from "./components/CustomCursor/CustomCursor.tsx";
-/* import Footer from "./components/Footer/Footer.tsx"; */
-import { BrowserRouter /* useLocation */ } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Menu from "./components/Menu/Menu.tsx";
 
 export function Root() {
   /*   const location = useLocation(); */
   const [menu, setMenu] = useState(false);
 
-  /*   // Define the paths where you don't want the footer
-  const pathsWithoutFooter = ["/Projetos", "/Projeto"]; */
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      window.location.reload();
+    };
+
+    window.addEventListener("orientationchange", handleOrientationChange);
+    return () =>
+      window.removeEventListener("orientationchange", handleOrientationChange);
+  }, []);
 
   return (
     <>
@@ -24,8 +30,6 @@ export function Root() {
       <Header menu={menu} setMenu={setMenu} />
       <Menu menu={menu} setMenu={setMenu} />
       <App menu={menu} />
-      {/* Only render the footer if the current path is not in the excluded list */}
-      {/*       {!pathsWithoutFooter.includes(location.pathname) && <Footer />} */}
     </>
   );
 }
